afe3b062bfc018ef017fd929ad48225bc7964b3b
Abeysiriwardan <miuru.2019450@iit.ac.lk>
2022-04-01 00:50:40 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# syntax=docker/dockerfile:1
+
+FROM python:3.9.7
+
+RUN pip install pipenv
+
+ENV PROJECT_DIR /UrHeart--SE-47-Backend
+
+WORKDIR ${PROJECT_DIR}
+
+COPY Pipfile Pipfile.lock ${PROJECT_DIR}/
+
+RUN pipenv install --system --deploy
---
+++ app.py
@@ -0,0 +1,47 @@
+from distutils.log import debug
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from check.checker import PredictionAPI
+from flask_cors import CORS
+# from resources.routes import initialize_routes
+import json
+
+
+app=Flask(__name__)
+api=Api(app)
+
+
+# class CoreApi(Resource):
+@app.route('/')
+def home():
+    return "UrHeart"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    test_data = request.json
+    s1 = json.dumps(test_data)
+    print_json = json.loads(s1)
+    list = print_json['body']
+    input_data = (  float(list['age']), 
+                    float(list['sex']), 
+                    float(list['chestPainType']),	
+                    float(list['restingBP']), 
+                    float(list['cholestrol']), 
+                    float(list['fastingBloodSugar']), 
+                    float(list['restingECG']), 
+                    float(list['maxHeartRate']), 
+                    float(list['exerciseAngina']),	
+                    float(list['oldpeak']), 
+                    float(list['STslope']))
+    return jsonify({'predResult':str(PredictionAPI(input_data))})
+
+
+@app.route('/test')
+def test():
+    return 'test'
+
+# initialize_routes(api)
+
+if __name__ == "__main__":
+    app.run(debug=False)
